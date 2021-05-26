ebef9175723b276a1df3555ffc20715ca32e2b8a
Alina Kotlyarova <84326673+AlinaKotlyarova@users.noreply.github.com>
2021-05-27 01:36:59 +0600

    правки по комментариям

--- tests/acceptance/ItemsCest.php
+++ tests/acceptance/ItemsCest.php
@@ -4,28 +4,19 @@ class ItemsCest
 {
      /**
      * Проверка формы быстрого просмотра карточки товара
-     *
      */
     public function checkQuickView(AcceptanceTester $I)
     {
         $I->amOnPage('');
-
-        $I->seeElement('#homefeatured > li:nth-child(2) .product-name');
+        $I->scrollTo('#center_column');
+        $I->waitForElementVisible('#homefeatured > li:nth-child(2) .product-name');
         $productName = $I->grabAttributeFrom('#homefeatured > li:nth-child(2) .product-name', 'title');
-
-        $I->seeElement('#homefeatured > li:nth-child(2) .quick-view');
+        $I->moveMouseOver('#homefeatured > li:nth-child(2)');
+        $I->waitForElementVisible('#homefeatured > li:nth-child(2) .quick-view');
         $I->click('#homefeatured > li:nth-child(2) .quick-view');
-
-        //нет функции waitForElementVisible, поэтому хотела поставить задержку 
-        //что бы он ждал пока откроется модалка
-        $I->seeElement('.fancybox-overlay-fixed');
-
-        //пытаюсь переключиться на iframe, не смогла найти подходяший селектор,
-        //пробовала еще .fancybox-iframe
-        $I->switchToIframe('#index > div.fancybox-overlay.fancybox-overlay-fixed > div > div > div > div');
-        $I->seeElement('#index > div.fancybox-overlay.fancybox-overlay-fixed > div > div > div > div');
-        $I->seeElement('#product > div > div > div.pb-center-column.col-xs-12.col-sm-4 > h1', $productName );
-       // был вариант словить селектор иконки загрузки 
-       //перед модалкой быстрого просмотра, но не смогла его выловить
+        $I->waitForElementVisible('#index > div.fancybox-overlay.fancybox-overlay-fixed > div > div');
+        $I->switchToIFrame(".fancybox-iframe");
+        codecept_debug($I->grabTextFrom( '#product > div > div > div.pb-center-column.col-xs-12.col-sm-4 > h1'));
+        $I->see($productName);
     }
 }
